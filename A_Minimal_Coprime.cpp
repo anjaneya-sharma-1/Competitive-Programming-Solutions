9dc509bbc81e807cf426e7887b4280c73dadc520
Anjaneya Sharma <23103301@MAIL.JIIT.AC.IN>
2025-01-29 20:47:59 +0530

    29-01-2025

---
+++ A_Minimal_Coprime.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define N 1000000
+#define MOD 1000000007
+class Solution{
+    public:
+void solve(){
+    ll l,r;
+    cin>>l>>r;
+    if(l==r&& r==1){
+        cout<<1<<endl;
+        return;
+    }
+    cout<<r-l<<endl;
+}
+
+};
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    ll t;
+    cin >> t;
+    
+    
+    while (t--){ 
+        Solution* s = new Solution(); 
+        s->solve();
+        delete s; 
+
+        }
+    return 0;
+}
