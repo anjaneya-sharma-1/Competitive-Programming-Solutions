bc8ac02373fc0074e14a76e4070d2ad422ca9b67
Anjaneya Sharma <23103301@MAIL.JIIT.AC.IN>
2025-01-31 19:47:47 +0530

    31-01-2025

---
+++ A_Kevin_and_Arithmetic.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define N 1000000
+#define MOD 1000000007
+class Solution{
+    public:
+void solve() {
+    ll n;
+    cin>>n;
+    ll odd=0;
+    for(int i=0;i<n;i++){
+        ll a;
+        cin>>a;
+        odd+=a%2;
+    }
+    ll ans=min(n-1,odd)+min(1LL,n-odd);
+    cout<<ans<<endl;
+    
+    
+}
+
+};
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    ll t;
+    cin >> t;
+    
+    
+    while (t--){ 
+        Solution* s = new Solution(); 
+        s->solve();
+        delete s; 
+
+        }
+    return 0;
+}
