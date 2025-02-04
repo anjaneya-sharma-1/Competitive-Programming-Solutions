760908be5484d80df8967b0934f30f9f8f3c9654
Anjaneya Sharma <23103301@MAIL.JIIT.AC.IN>
2025-02-04 20:08:37 +0530

    04-02-2025

---
+++ A_Kevin_and_Combination_Lock.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define N 1000000
+#define MOD 1000000007
+class Solution{
+    public:
+void solve() {
+    int x;
+    std::cin >> x;
+    
+    if (x % 33 == 0) {
+        std::cout << "YES\n";
+    } else {
+        std::cout << "NO\n";
+    }
+    
+}
+
+};
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    ll t;
+    cin >> t;
+    
+    
+    while (t--){ 
+        Solution* s = new Solution(); 
+        s->solve();
+        delete s; 
+
+        }
+    return 0;
+}
