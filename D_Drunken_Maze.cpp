f2964050419ec1e7d865121f072d1ae72b2553ab
Anjaneya Sharma <23103301@MAIL.JIIT.AC.IN>
2025-02-11 18:38:18 +0530

    11-02-2025

--- D_Drunken_Maze.cpp
+++ D_Drunken_Maze.cpp
@@ -3,88 +3,69 @@ using namespace std;
 #define ll long long
 #define N 1000000
 #define MOD 1000000007
-class Solution{
-    public:
-void solve() {
-    ll n,m;
-    cin>>n>>m;
-    vector<string>grid(n);
-    pair<ll,ll>start;
-    pair<ll,ll>end;
-    for(int i=0;i<n;i++){
-        cin>>grid[i];
-        for(int j=0;j<m;j++){
-            if(grid[i][j]=='S'){
-                start={i,j};
+
+
+class Solution {
+public:
+    void solve() {
+        ll n, m;
+        cin >> n >> m;
+        vector<string> grid(n);
+        pair<ll,ll> start, end;
+        
+        // Read input
+        for(int i = 0; i < n; i++) {
+            cin >> grid[i];
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 'S') start = {i,j};
+                if(grid[i][j] == 'T') end = {i,j};
             }
-            if(grid[i][j]=='T'){
-                end={i,j};
+        }
+
+        
+        set<vector<ll>> visited;
+     
+        queue<pair<ll, vector<ll>>> q;
+        
+        
+        q.push({0, {start.first, start.second, -1, 0}});
+        
+        vector<pair<ll,ll>> dirs = {{-1,0}, {1,0}, {0,-1}, {0,1}}; // up, down, left, right
+        
+        while(!q.empty()) {
+            auto [dist, state] = q.front();
+            q.pop();
+            
+            ll x = state[0], y = state[1];
+            ll last_dir = state[2];
+            ll consec = state[3];
+            
+            if(x == end.first && y == end.second) {
+                cout << dist << endl;
+                return;
+            }
+            
+            for(ll dir = 0; dir < 4; dir++) {
+                ll nx = x + dirs[dir].first;
+                ll ny = y + dirs[dir].second;
+                
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m || grid[nx][ny] == '#') 
+                    continue;
+                
+                ll new_consec = (dir == last_dir) ? consec + 1 : 1;
+                
+                if(new_consec > 3) continue;
+                
+                vector<ll> new_state = {nx, ny, dir, new_consec};
+                if(visited.count(new_state)) continue;
+                
+                visited.insert(new_state);
+                q.push({dist + 1, new_state});
             }
         }
+        
+        cout << -1 << endl;
     }
-    vector<vector<ll>>dist(n,vector<ll>(m,INT_MAX));
-    priority_queue<pair<ll,pair<vector<ll>,pair<ll,ll>>>,vector<pair<ll,pair<vector<ll>,pair<ll,ll>>>>,greater<pair<ll,pair<vector<ll>,pair<ll,ll>>>>>pq;
-    pq.push({0,{vector<ll>(4,0),start}});
-    while(!pq.empty()){
-        auto curr=pq.top();
-        pq.pop();
-        ll d=curr.first;
-        vector<ll>dir=curr.second.first;
-        pair<ll,ll>pos=curr.second.second;
-        ll i=pos.first;
-        ll j=pos.second;
-        if(dist[i][j]<=d){
-            continue;
-        }
-        dist[i][j]=d;
-        if(pos==end){
-            cout<<d<<endl;
-            return;
-        }
-         vector<ll>temp=dir;
-        if(i-1>=0&&grid[i-1][j]!='#'&&dir[0]<3){
-           
-            temp[0]++;
-            temp[1]=0;
-            temp[2]=0;
-            temp[3]=0;
-            pq.push({d+1,{temp,{i-1,j}}});
-            temp={dir};
-        }
-        if(i+1<n&&grid[i+1][j]!='#'&&dir[1]<3){
-            
-            temp[1]++;
-            temp[0]=0;
-            temp[2]=0;
-            temp[3]=0;
-
-            pq.push({d+1,{temp,{i+1,j}}});
-            temp={dir};
-        }
-        if(j-1>=0&&grid[i][j-1]!='#'&&dir[2]<3){
-            
-            temp[2]++;
-            temp[0]=0;
-            temp[1]=0;
-            temp[3]=0;
-            pq.push({d+1,{temp,{i,j-1}}});
-            temp={dir};
-        }
-        if(j+1<m&&grid[i][j+1]!='#'&&dir[3]<3){
-            
-            temp[3]++;
-            temp[0]=0;
-            temp[1]=0;
-            temp[2]=0;
-            pq.push({d+1,{temp,{i,j+1}}});
-            temp={dir};
-        }
-
-    }
-    cout<<-1<<endl;
-    
-}
-
 };
 int main(){
     std::ios::sync_with_stdio(false);
