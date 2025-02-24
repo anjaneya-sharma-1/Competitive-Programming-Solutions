3f4ae9aad84849d4d1bee7d5ec3fb891f177aeca
Anjaneya Sharma <23103301@MAIL.JIIT.AC.IN>
2025-02-24 21:30:14 +0530

    24-0-2025

---
+++ A_Watermelon.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define N 1000000
+#define MOD 1000000007
+class Solution{
+    public:
+void solve() {
+    ll n;
+    cin>>n;
+    cout<<((n%2==0&&n>2)?"YES":"NO")<<endl;
+    
+}
+
+};
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    ll t=1;
+    
+    
+    while (t--){ 
+        Solution* s = new Solution(); 
+        s->solve();
+        delete s; 
+
+        }
+    return 0;
+}
--- C_Beautiful_Sequence.cpp
+++ C_Beautiful_Sequence.cpp
@@ -6,37 +6,26 @@ using namespace std;
 class Solution{
     public:
 void solve() {
-    ll n;
-    cin>>n;
-    vector<ll>v(n);
-    for(ll i=0;i<n;i++){
-        cin>>v[i];
-    }
-    ll ans=0;
-    ll curr=0;
-    ll ok=0;
-    ll count=0;
-    ll buff=0;
-    for(int i=0;i<n;i++){
-        if(v[i]==1){
-            ok=1;
-            buff++;
-        }
-        else if(v[i]==2&&ok){
-            curr=(curr*2)%MOD;
-            if(buff){
-                curr=(curr+(2*buff)%MOD)%MOD;
-                count+=buff;
-                buff=0;
-            }
-            curr%=MOD;
-        }
-        else if(v[i]==3){
-            ans+=curr-count;
-            ans%=MOD;
+    int n;
+    std::cin >> n;
+    
+    ll ans = 0;
+    ll p = 0;
+    ll q = 0;
+    for (int i = 0; i < n; i++) {
+        int x;
+        std::cin >> x;
+        if (x == 1) {
+            p += 1;
+            q += 1;
+        } else if (x == 2) {
+            p *= 2;
+        } else {
+            ans += p - q;
         }
     }
-    cout<<ans<<endl;
+    std::cout << ans << "\n";
+
     
 }
 
