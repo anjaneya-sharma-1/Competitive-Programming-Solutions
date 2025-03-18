ce95c0cad37c29d465899b562e3b7a044ed0563c
Anjaneya Sharma <23103301@MAIL.JIIT.AC.IN>
2025-03-18 12:56:14 +0530

    18-03-2025

---
+++ B_Stoned_Game.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define N 1000000
+#define MOD 1000000007
+class Solution{
+    public:
+void solve() {
+    ll n;
+    cin>>n;
+    vector<ll>a(n);
+    ll sum=0;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        sum+=a[i];
+    }
+    for(int i=0;i<n;i++){
+        if(a[i]>sum-a[i]){
+            cout<<"T"<<endl;
+            return;
+        }
+    }
+    cout<<(sum%2==0?"HL":"T")<<endl;
+
+    
+}
+
+};
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    ll t;
+    cin >> t;
+    
+    
+    while (t--){ 
+        Solution* s = new Solution(); 
+        s->solve();
+        delete s; 
+
+        }
+    return 0;
+}
