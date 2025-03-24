1937ad0a8a13be02743df9133bdc19f8253d4ddd
Anjaneya Sharma <23103301@MAIL.JIIT.AC.IN>
2025-03-24 17:08:48 +0530

    24-03-2025

---
+++ A_Halloumi_Boxes.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define N 1000000
+#define MOD 1000000007
+class Solution{
+    public:
+void solve() {
+    ll n,k;
+    cin>>n>>k;
+    vector<ll>arr(n);
+    for(ll i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    vector<ll>s=arr;
+    sort(s.begin(),s.end());
+
+    if(arr==s){
+        cout<<"YES"<<endl;
+        return;
+    }
+    else if(k<=1){
+        cout<<"NO"<<endl;
+        return;
+    }
+    cout<<"YES"<<endl;
+    
+}
+
+};
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    ll t;
+    cin >> t;
+    
+    
+    while (t--){ 
+        Solution* s = new Solution(); 
+        s->solve();
+      
+
+        }
+    return 0;
+}
