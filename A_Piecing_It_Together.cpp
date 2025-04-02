e41fd5ffa7407336daa1eda1a06f58cd6e9fac2b
Anjaneya Sharma <23103301@MAIL.JIIT.AC.IN>
2025-04-02 17:54:35 +0530

    02-04-2025

---
+++ A_Piecing_It_Together.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define N 1000000
+#define MOD 1000000007
+class Solution{
+    public:
+void solve() {
+    cout<<"puzzling"<<endl;
+    
+}
+
+};
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    ll t=1;
+    
+    
+    while (t--){ 
+        Solution* s = new Solution(); 
+        s->solve();
+        delete s; 
+
+        }
+    return 0;
+}
