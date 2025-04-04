45ce35802ae5ee7cb169aa26ab571ae93264c5d2
Anjaneya Sharma <23103301@MAIL.JIIT.AC.IN>
2025-04-04 22:31:52 +0530

    04-04-2025

---
+++ B_Plinko.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define int long long
+ 
+void solve()
+{
+    string s;
+    cin >> s;
+    int n; cin >> n;
+    cout << 17;
+    return;
+}
+ 
+int32_t main()
+{
+    // int t;
+    // cin >> t;
+    // while (t--){
+        solve();
+    //     cout << endl;
+    // }
+    return 0;
+}
---
+++ D_Cleaning_the_Phone.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define N 1000000
+#define MOD 1000000007
+class Solution{
+    public:
+void solve() {
+    ll n,m;
+    cin>>n>>m;
+    vector<ll>val(n);
+    vector<ll>a;
+    vector<ll>b;
+    for(int i=0;i<n;i++){
+        cin>>val[i];
+    }
+    for(int j=0;j<n;j++){
+        ll x;
+        cin>>x;
+        if(x==1){
+            a.push_back(val[j]);
+        }else{
+            b.push_back(val[j]);
+        }
+    }
+    sort(a.begin(),a.end(),greater<ll>());
+    sort(b.begin(),b.end(),greater<ll>());
+    ll sum=0;
+    ll ans=INT_MAX;
+    if(accumulate(a.begin(),a.end(),0)+accumulate(b.begin(),b.end(),0)<m){
+        cout<<-1<<"\n";
+        return;
+    }
+
+    ll bsum=0;
+    ll j=0;
+    for(j=0;j<b.size();j++){
+        bsum+=b[j];
+        if(bsum>=m){
+            ans=min(ans,2*(j+1));
+            break;
+        }
+    }
+    for(int i=0;i<a.size();i++){
+        bsum+=a[i];
+        while(j>=0&&bsum-b[j]>=m){
+            bsum-=b[j];
+            j--;
+        }
+        if(bsum>=m){
+            ans=min(ans,(i+1)+2*(j+1));
+        }
+    }
+    cout<<ans<<"\n";
+    return;
+    
+    
+}
+
+};
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    ll t;
+    cin >> t;
+    
+    
+    while (t--){ 
+        Solution* s = new Solution(); 
+        s->solve();
+      
+
+        }
+    return 0;
+}
