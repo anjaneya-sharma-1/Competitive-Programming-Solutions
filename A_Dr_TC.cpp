508af7f863213034c5eeb147643163d4addec7cf
Anjaneya Sharma <23103301@MAIL.JIIT.AC.IN>
2025-04-25 20:27:12 +0530

    25-04-2025

---
+++ A_Dr_TC.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define N 1000000
+#define MOD 1000000007
+class Solution{
+    public:
+void solve() {
+    ll n;
+    cin>>n;
+    string s;
+    cin>>s;
+    ll ans=n+ count(s.begin(),s.end(),'1')*(n-2);
+    cout<<ans<<endl;
+    
+}
+
+};
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    ll t;
+    cin >> t;
+    
+    
+    while (t--){ 
+        Solution* s = new Solution(); 
+        s->solve();
+      
+
+        }
+    return 0;
+}
